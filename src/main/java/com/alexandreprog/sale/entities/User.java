8ef8e14b744bf2519b62d51cef327c9f9856dbde
Alexandre Sampaio <alexandresampaioprog@gmail.com>
2024-09-02 17:25:34 -0300

    H2 database, test profile, JPA

--- src/main/java/com/alexandreprog/sale/entities/User.java
+++ src/main/java/com/alexandreprog/sale/entities/User.java
@@ -3,10 +3,21 @@ package com.alexandreprog.sale.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name="tb_user")
 public class User implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
@@ -15,7 +26,8 @@ public class User implements Serializable{
 	
 	public User() {
 	}
-
+	
+	
 	public User(Long id, String nome, String email, String phone, String password) {
 		super();
 		this.id = id;
