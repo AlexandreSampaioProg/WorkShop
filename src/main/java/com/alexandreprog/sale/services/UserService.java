4669dee56ed312d15bdac262cc5f4d239bc52fd9
Alexandre Sampaio <alexandresampaioprog@gmail.com>
2024-10-06 02:11:23 -0300

    Exception handling - delete

--- src/main/java/com/alexandreprog/sale/services/UserService.java
+++ src/main/java/com/alexandreprog/sale/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.alexandreprog.sale.entities.User;
 import com.alexandreprog.sale.repositories.UserRepository;
+import com.alexandreprog.sale.services.exception.DatabaseException;
 import com.alexandreprog.sale.services.exception.ResourceNotFoundException;
 
 
@@ -31,7 +34,14 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public User update(Long id, User obj) {
